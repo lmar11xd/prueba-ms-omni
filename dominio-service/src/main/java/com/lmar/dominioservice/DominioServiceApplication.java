9c19efd9002ebac9c464dfd821021b06e942a86b
Luis Miguel Alvarado <lmar11xd@gmail.com>
2023-09-30 12:22:39 -0500

    Configuracion terminada. Todos los servicios levantados

--- dominio-service/src/main/java/com/lmar/dominioservice/DominioServiceApplication.java
+++ dominio-service/src/main/java/com/lmar/dominioservice/DominioServiceApplication.java
@@ -2,8 +2,10 @@ package com.lmar.dominioservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DominioServiceApplication {
 
 	public static void main(String[] args) {
